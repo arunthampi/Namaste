d0a7443ea6587c5a5ba9c4abc1e303f52c01c119
arunthampi <arun.thampi@gmail.com>
2009-07-21 22:38:44 +0800

    Initial Commit for Namaste

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Namaste
+//
+//  Created by Arun Thampi on 7/21/09.
+//  Copyright Bezurk 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
