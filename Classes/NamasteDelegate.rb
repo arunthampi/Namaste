fbe36ea6b78c66d9de6e534d7221b37c47daf5ea
arunthampi <arun.thampi@gmail.com>
2009-07-21 23:13:32 +0800

    First Delegate

---
+++ Classes/NamasteDelegate.rb
@@ -0,0 +1,13 @@
+#
+#  NamasteDelegate.rb
+#  Namaste
+#
+#  Created by Arun Thampi on 7/21/09.
+#  Copyright (c) 2009 Bezurk. All rights reserved.
+#
+
+class NamasteDelegate
+  def applicationDidFinishLaunching(notification)
+    NSLog "Namaste!"
+  end
+end
