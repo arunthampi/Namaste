757c3d7ef41edadfb9b4c2755bedc37b30f32416
arunthampi <arun.thampi@gmail.com>
2009-07-22 07:51:33 +0800

    Add WebKit framework and WebView

--- Classes/NamasteDelegate.rb
+++ Classes/NamasteDelegate.rb
@@ -7,7 +7,14 @@
 #
 
 class NamasteDelegate
+  attr_accessor :web_view
+  attr_accessor :url_field
+  attr_accessor :google_search_field
+  
+  
   def applicationDidFinishLaunching(notification)
-    NSLog "Namaste!"
+    NSLog "Namaste! Going to launch a default website"
+    web_view.mainFrame.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString("http://www.wego.com")))
+    NSLog "Finished giving instruction to load the page"
   end
 end
