bf049b75a727c89a81d47acfa7146d1593b9699a
arunthampi <arun.thampi@gmail.com>
2009-07-23 11:53:58 +0800

    Get both the URL field as well as the Google Search Field to respond only to return key events, delegates ftw

--- Classes/NamasteDelegate.rb
+++ Classes/NamasteDelegate.rb
@@ -3,7 +3,7 @@
 #  Namaste
 #
 #  Created by Arun Thampi on 7/21/09.
-#  Copyright (c) 2009 Bezurk. All rights reserved.
+#  Copyright (c) 2009. All rights reserved.
 #
 require 'uri'
 
@@ -12,23 +12,34 @@ class NamasteDelegate
   attr_accessor :url_field
   attr_accessor :google_search_field
   
-  
   def applicationDidFinishLaunching(notification)
+    # Set the delegate of the URL Field to be NamasteDelegate,
+    # so that we can handle events
+    url_field.delegate = self
+    google_search_field.delegate = self
+    # Introductory log
     NSLog("Namaste!")
   end
   
-  def loadURL(sender)
-    url = sender.stringValue
-    url = "http://#{url}" if url.index('http:\/\/').nil?
-    
-    NSLog("Going to log url: #{url}")
-    web_view.mainFrame.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString(url)))
-  end
-  
-  def loadGoogleSearchPage(sender)
-    search_term = URI.encode(sender.stringValue)
-    url = "http://www.google.com/search?rls=en-us&q=#{search_term}&ie=UTF-8&oe=UTF-8"
-    web_view.mainFrame.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString(url)))
+  def controlTextDidEndEditing(notification)
+    if(notification.userInfo['NSTextMovement'] == NSReturnTextMovement)
+      if notification.object == url_field
+        url = url_field.stringValue
+        unless url.nil? || url.length == 0
+          url = "http://#{url}" if url.index('http:\/\/').nil?
+        end
+      elsif notification.object == google_search_field
+        search_term  = google_search_field.stringValue
+        unless search_term.nil? || search_term.length == 0
+          url = "http://www.google.com/search?rls=en-us&q=#{URI.encode(search_term)}&ie=UTF-8&oe=UTF-8"
+        end
+      end
+      
+      if url # Only if the URL is set, do soemthing, otherwise sit still.
+        NSLog "Fetching the URL: #{url}"
+        web_view.mainFrame.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString(url)))
+      end
+    end
   end
   
 end
